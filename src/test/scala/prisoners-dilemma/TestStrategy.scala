d658c98cea733280ce670aab14e773aecdda807d
Jessica Kerr <jessitron@gmail.com>
2014-07-08 23:28:56 -0500

    Wait a minute. This should be easier > > I think I need to pass an iterator, rather than a sequence I could update. It really is something > that I want the items to go away from as they're used. > > The TestRound I expect to be called synchronously with the possible exception of it could be re-called before a wait is up > But the newGame method will be called a whole bunch at once. > > Really what it needs is something that, once drawn from, that instruction > is exhausted. Yet it's not a stack because it's potentially infinite. Should be infinite, in fact. > It's a freaking Iterator. > Are they thread-safe, when they come out of a Stream?

--- src/test/scala/prisoners-dilemma/TestStrategy.scala
+++ src/test/scala/prisoners-dilemma/TestStrategy.scala
@@ -6,50 +6,55 @@ import java.util.concurrent.atomic.AtomicReference
 
 object TestStrategy {
 
-  trait Instruction
-  case class TakeMove(m: Move) extends Instruction
-  case class WaitAndThen(d: Duration) extends Instruction
-  case object Failinate extends Instruction
+  trait Instruction[T]
+  case class WhatYouNeed[T](m: T) extends Instruction[T]
+  case class WaitAndThen(d: Duration) extends Instruction[Nothing]
+  case object Failinate extends Instruction[Nothing]
 
+  type RoundInstruction = Instruction[Move]
 
-  class TestRound(thisMove: Move, future: Seq[Instruction]) extends RoundStrategy {
+  class TestRound(thisMove: Move, future: Seq[RoundInstruction]) extends RoundStrategy {
 
     var instructions = new AtomicReference(future)
 
     def currentMove = thisMove
-    def next(oppMove: Move) = follow(instructions.get, (is) => instructions.set(is))
+    def next(oppMove: Move) = follow(instructions.get,
+      (m, rest) => TestRound(m, rest)
+      (is) => instructions.set(is))
   }
 
-  private def follow(instr: Seq[Instruction], remainder: Seq[Instruction] => Unit): TestRound= {
+
+  private def follow[T, R](instr: Seq[Instruction[T]],
+                           finish: (T, Seq[Instruction[T]]) => R,
+                           beforeException: Seq[Instruction[T]] => Unit): R = {
         if (instr.isEmpty) throw new Exception("End of instruction set")
         instr.head match {
-         case TakeMove(m) => new TestRound(m, instr.tail)
+         case WhatYouNeed(m) => finish(m, instr.tail)
          case WaitAndThen(d) => blocking {
            Thread.sleep(d.toMillis)
           }
           follow(instr.tail, remainder)
          case Failinate =>
-          remainder(instr.tail)
+          beforeException(instr.tail)
           // can't recurse when I'm about to...
           throw new Exception("POOOOOOOOP")
        }
       }
 
-  class TestStrategizer(stuff: Seq[Seq[Instruction]]) extends Strategizer {
+      type StrategizerInstruction = Instruction[RoundInstruction]
+  class TestStrategizer(stuff: Seq[StrategizerInstruction]) extends Strategizer {
 
     var instructionSeqs = new AtomicReference(stuff)
 
      def newGame() = {
-       if (instructionSeqs.isEmpty) {
-         throw new Exception("No more instruction sets")
-       }
-       val thisStuff = instructionSeqs.head
+       val myInstructions = instructionSeqs.wait....
+       val result = follow(instructionSeqs.get,
+         (newGame, remainder) => {
+           instructionSeqs.set(remainder)
+           newGame
+         }
+         instructionSeqs.set _)
 
-       val result = follow(thisStuff,
-         // in case of exception, I want to re-use the rest of this instruction sequence
-         is => instructionSeqs.set(is :: instructionSeqs.tail ))
-
-       instructionSeqs.set(instructionSeqs.tail)
        result
      }
   }
